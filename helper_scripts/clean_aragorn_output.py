ab8565c86280ca7d8d8d76557d213fea4eaf9781
blue-moon22 <victoriacarr018@gmail.com>
2019-11-13 14:21:34 +0000

    remove newline from input aragorn output

--- helper_scripts/clean_aragorn_output.py
+++ helper_scripts/clean_aragorn_output.py
@@ -2,7 +2,7 @@ fin = '../data/megaphage_trna.tab'
 fout = '../data/megaphage_trna_clean.tab'
 
 wr = open(fout, 'w')
-with open(fin, 'r') as infile:
+with open(fin, 'r') as infile:c
     for line in infile:
         if line[0] == '>':
             id = line.split('>')[1].split('\n')[0]
