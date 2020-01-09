777a46ffc912385fa124333d5027967e086db0ce
blue-moon22 <victoriacarr018@gmail.com>
2020-01-09 17:46:39 +0000

    fix proportion of mapped reads in vir_counts_prop_melt

--- helper_scripts/preprocessing.R
+++ helper_scripts/preprocessing.R
@@ -436,10 +436,10 @@ vir_coverage = vir_coverage[rownames(vir_counts), colnames(vir_counts)]
 vir_counts[vir_coverage < 0.75] = 0 #Apply breadth of coverage filter (<75% coverage are removed)
 # rm(vir_coverage)
 
-# vir_counts_unaligned = counts_total - apply(vir_counts, 2, sum)
-# vir_counts = rbind(vir_counts, vir_counts_unaligned)
-vir_counts = vir_counts[-nrow(vir_counts),]
+vir_counts_unaligned = counts_total - apply(vir_counts, 2, sum)
+vir_counts = rbind(vir_counts, vir_counts_unaligned)
 vir_counts_prop = apply(vir_counts, 2, function(x) x/sum(x))
+vir_counts_prop = vir_counts_prop[-nrow(vir_counts_prop),]
 # rm(vir_counts)
 
 #Remove samples with missing total counts
@@ -459,17 +459,13 @@ contig_data <- readRDS("../data/contig_data.RDS")
 vir_counts_prop_melt = melt(vir_counts_prop)
 # rm(vir_counts_prop)
 vir_counts_prop_melt <- left_join(vir_counts_prop_melt, contig_data[,c("name", "demovir", "vcontact_cluster", "crispr_host", "integrase")], by = c("Var1"="name"))
-
-# Aggregate counts by Demovir/vConTACT2
 vir_counts_prop_melt <- vir_counts_prop_melt %>% filter(value != 0)
 
 # Add metadata
 vir_counts_prop_melt <- left_join(vir_counts_prop_melt, metadata[,c("ID", "sample_type", "Location")], by = c("Var2"="ID"))
 saveRDS(vir_counts_prop_melt, file = "../data/vir_counts_prop_melt.RDS")
 
-# Aggregate by ID
-vir_counts_prop_melt_agg <- vir_counts_prop_melt %>% group_by(Var2, demovir, vcontact_cluster, sample_type, Location) %>%
-  summarise(V1 = sum(value))
+# Aggregate counts by Demovir/vConTACT2
 vir_counts_prop_melt <- as.data.table(vir_counts_prop_melt)
 vir_counts_prop_melt_agg = vir_counts_prop_melt[, sum(value), by=.(Var2, demovir, vcontact_cluster, sample_type, Location)]
 saveRDS(vir_counts_prop_melt_agg,  file = "../data/vir_counts_prop_melt_agg.RDS")
